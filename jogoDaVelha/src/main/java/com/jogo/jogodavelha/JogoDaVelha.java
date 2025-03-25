18b5bf8ec8b7e625a055a8ed6e54f586e3123efa
chris-20mb <christophermachado222@gmail.com>
2025-03-25 15:17:56 -0300

    first commit

---
+++ jogoDaVelha/src/main/java/com/jogo/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jogo.jogodavelha;
+
+/**
+ *
+ * @author chris
+ */
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+        Jogo FrameJogo = new Jogo();
+        FrameJogo.setVisible(true);
+    }
+}
